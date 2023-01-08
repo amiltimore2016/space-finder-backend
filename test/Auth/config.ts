4a5d2f87f26fb57556614dcbf0ef7bed60300f4d
Antonio Manuel Medina Miltimore <3soft4you@gmail.com>
2023-01-08 19:47:20 +0000

    added cognito auth

---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    REGION: 'eu-west-2',
+    USER_POOL_Id: 'eu-west-2_do2QIgpQY',
+    APP_CLIENT_ID: '75917dfqpnvcmmkopf14jt1qh0',
+    TEST_USER_NAME: 'testuser1',
+    TEST_USER_PASSWORD: 'iC@eetoo0'
+}
